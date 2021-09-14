84107797ea8a16718b04adcd6b2d4404ab597be4
jerald.s.gutierrez <gutierrez.jeralds@yahoo.com>
2021-09-14 17:05:58 +0800

    Enable auto page refresh

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -98,7 +98,7 @@ class Home extends React.Component {
     // Page reload
     pageRefresh = (time) => {
         setTimeout( function() {
-            // window.location.reload();
+            window.location.reload();
         }, time);
     }
 

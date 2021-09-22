d04eb38646822d0bb7fd63a8fdba417df1087969
jerald.s.gutierrez <gutierrez.jeralds@yahoo.com>
2021-09-22 22:12:27 +0800

    Adding comment

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -23,7 +23,7 @@ class Home extends React.Component {
             notifStr: "",
             isUser: this.props.user || "",
             isSponsorName: "",
-            managerPHPInvestment: 337000,
+            managerPHPInvestment: 337000, // Estimated Investment
             managerPHPROI: 0,
             managerPHPBreed: 0,
             managerPHPBuy: 0,

fba84b5a14ae59ac30b21d8f332e2456b49b93c8
jerald.s.gutierrez <gutierrez.jeralds@yahoo.com>
2022-11-15 19:08:28 +0800

    Revert changes in Main x Remove BrowserRoute

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import Classic from './views/classic/Home' // Home page for Classic details
-import Origin from './views/origin/Home' // Home page for Origin details
+import Home from './views/origin/Home' // Home page for Origin details
 import Login from './views/Login'
 import Cookies from 'js-cookie'
 import { MDBBox } from 'mdbreact'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
 class Main extends React.Component {
@@ -21,20 +19,11 @@ class Main extends React.Component {
         if ( this.state.promptUser !== null && this.state.promptUser !== undefined && this.state.promptUser !== "" ) {
             return (
                 <React.Fragment>
-                    <Router>
-                        <Header user={this.state.promptUser} />
-                        <MDBBox tag="div" className="main">
-                            <Switch >
-                                <Route exact path="/">
-                                    <Origin user={this.state.promptUser} />
-                                </Route>
-                                <Route exact path="/classic">
-                                    <Classic user={this.state.promptUser} />
-                                </Route>
-                            </Switch>
-                        </MDBBox>
-                        <Footer />
-                    </Router>
+                    <Header user={this.state.promptUser} />
+                    <MDBBox tag="div" className="main">
+                        <Home user={this.state.promptUser} />
+                    </MDBBox>
+                    <Footer />
                 </React.Fragment>
             )
         } else {

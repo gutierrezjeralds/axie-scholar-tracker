4f652bbb8e4bfbf9c5d2ab39912ac1b6ff33c326
jerald.s.gutierrez <gutierrez.jeralds@yahoo.com>
2022-11-18 11:22:42 +0800

    Update option property of mongodb conn

--- nodejs/dbconn/dbconn.js
+++ nodejs/dbconn/dbconn.js
@@ -6,11 +6,13 @@ const { MongoClient } = require('mongodb');
 // const connectionString = process.env.ATLAS_URI;
 const connectionString = process.env.MONGODB_URI;
 const client = new MongoClient(connectionString, {
-    connectTimeoutMS: 12000,
-    socketTimeoutMS: 12000,
-    serverSelectionTimeoutMS: 12000,
-    useNewUrlParser: true,
-    useUnifiedTopology: false,
+    useNewUrlParser: true, // Avoid deprecation warning
+    useUnifiedTopology: true, // Avoid deprecation warning
+    useCreateIndex: true, // False by default. Set to true to make Mongoose's default index build use createIndex() instead of ensureIndex() to avoid deprecation warnings from the MongoDB driver.
+    autoIndex: true, // We want indexes to be build
+    poolSize: 100, // Maintain up to 100 socket connections, default is 5
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 300000, // Close sockets after 5 minutes of inactivity
 });
 
 let dbConnection;
